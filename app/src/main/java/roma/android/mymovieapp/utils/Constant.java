2966ca9d190a2ecf75af732982e461293c983c9a
romaiswara <rhomajr27@gmail.com>
2020-01-22 00:07:31 +0700

    update

--- app/src/main/java/roma/android/mymovieapp/utils/Constant.java
+++ app/src/main/java/roma/android/mymovieapp/utils/Constant.java
@@ -3,7 +3,7 @@ package roma.android.mymovieapp.utils;
 public class Constant {
     public static final String BASE_URL = "https://api.themoviedb.org/3/movie/";
     public static final String API_KEY = "f837f2de7c33106b8476f5d8f75eff86";
-    public static final String API_KEY_YOUTUBE = "AIzaSyCKoN4VYaYE4e0qnKLe80x-gt8jITe6W3c";
+    public static final String API_KEY_YOUTUBE = "xxx";
     public static final String URLIMAGE = "https://image.tmdb.org/t/p/w780/";
     public static final String NOWPLAYING = "1";
     public static final String UPCOMING = "2";
